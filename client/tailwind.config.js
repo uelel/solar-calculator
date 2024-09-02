66e3d5cbb898dada84329a1b40b2bac66572a2c1
Uelel <j.ullsperger@digitrust.cz>
2024-09-02 12:12:02 +0200

    tailwind purge: all material-tailwind components added
    note: css size can be optimized by including only used material-tailwind components

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = withMT({
   purge: [
     "./src/**/*.html",
     "./src/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}"
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/components/MT/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@material-tailwind/react/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@material-tailwind/react/types/components/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {
