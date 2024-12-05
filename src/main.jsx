6be791dd29e9165ccc675d601582bb345a1c5672
Uelel <j.ullsperger@digitrust.cz>
2024-12-05 09:15:53 +0100

    vite added
    React Router removed

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App';
+
+import { ThemeProvider } from "@material-tailwind/react";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
